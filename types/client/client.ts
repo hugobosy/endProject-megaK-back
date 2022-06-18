458e1721c257e5257e17cba0a49f08220756a8c8
HugoBosy <hkliszcz@gmail.com>
2022-06-18 09:21:41 +0200

    add cilent types and client class

---
+++ types/client/client.ts
@@ -0,0 +1,12 @@
+export interface Client {
+    id: string,
+    name: string,
+    surname: string,
+    address: string,
+    code: string,
+    city: string,
+    phone: number,
+    gender: string,
+    birth: string,
+    mail: string,
+}
