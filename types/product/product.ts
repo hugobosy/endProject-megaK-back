03a1a1a1354a35ad5e6b1c5d54530bee3ed17a2f
HugoBosy <hkliszcz@gmail.com>
2022-06-20 11:22:05 +0200

    add types product

---
+++ types/product/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: string,
+    name: string,
+    model: string,
+    size: number,
+    color: string,
+    price: number,
+    quantity: number,
+    description: string,
+    category: string,
+    picture: string,
+}
