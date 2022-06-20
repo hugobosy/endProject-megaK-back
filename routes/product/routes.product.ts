89cbe99943f9fcb97215f5709dade04ba9ee478e
HugoBosy <hkliszcz@gmail.com>
2022-06-20 14:51:04 +0200

    modified query sql

--- routes/product/routes.product.ts
+++ routes/product/routes.product.ts
@@ -3,7 +3,6 @@ import {pool} from "../../utils/db";
 
 export const routesProduct = Router()
     .get('/', async (req, res) => {
-        const data = await pool.execute("SELECT products.id, products.firm, products.model, products.price, products.description, products.category, products.picture, category.name FROM `products` JOIN `category` ON products.category = category.id");
+        const data = await pool.execute("SELECT products.id, products.firm, products.model, products.price, products.description, products.category, products.picture, products.quantity, category.name FROM `products` JOIN `category` ON products.category = category.id");
         res.json(data[0]);
-        console.log(data[0])
     })
