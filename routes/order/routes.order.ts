d470b0e78cb4dc83db626e4cca66778952600aa0
HugoBosy <hkliszcz@gmail.com>
2022-06-30 10:27:18 +0200

    add query update sells table and update quantity product when order finished

--- routes/order/routes.order.ts
+++ routes/order/routes.order.ts
@@ -11,7 +11,8 @@ export const routesOrder = Router()
     .post('/simulate', async (req, res) => {
         const data = req.body;
         const buy = data.buy;
-
+        console.log(buy)
         await pool.execute("INSERT INTO `orders`(`id`, `date`, `total`, `payment`, `products`, `count`, `client`) VALUES (:id, :date, :total, :payment, :products, :count, :client)", data);
-        await buy.forEach((item: any) => pool.execute("INSERT INTO `sells`(`product_id`, `quantity_sells`)  VALUES (:id, :count)", item));
+        await buy.forEach((item: any) => pool.execute("UPDATE `sells` SET `quantity_sells` = sells.quantity_sells + :count WHERE `product_id`=:id", item));
+        await buy.forEach((item: any) => pool.execute("UPDATE `products` SET `quantity` = products.quantity - :count WHERE `id`=:id", item));
     })
